00045219a3d76702188b4a72e54878b5e40b211f
Gabriel <gabriel.fb13@puccampinas.edu.br>
2026-05-18 19:33:56 -0300

    rotas pis concluidas e testadas

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,14 @@
 import Fastify from "fastify";
 import { env } from "./env";
 import { healthRoutes } from "./routes/health";
+import { pisCofinRoutes } from './routes/pis-cofins';
 
 const app = Fastify({
   logger: true,
 });
 
 app.register(healthRoutes);
-
+app.register(pisCofinRoutes);
 app.listen({
   port: env.PORT,
 }).then(() => {
