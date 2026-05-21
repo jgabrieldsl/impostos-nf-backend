30f686fad6bf6a164fd7bc1e264e2fa55b6fdcc0
jgabrieldsl <jgabriel.leite@outlook.com>
2026-05-20 22:07:58 -0300

    fix: wrap swaggerPlugin with fastify-plugin to avoid routes omission in schema

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -1,8 +1,9 @@
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
 import type { FastifyInstance } from "fastify";
+import fp from "fastify-plugin";
 
-export async function swaggerPlugin(app: FastifyInstance) {
+async function swaggerPluginRaw(app: FastifyInstance) {
   await app.register(fastifySwagger, {
     openapi: {
       openapi: "3.0.0",
@@ -24,3 +25,7 @@ export async function swaggerPlugin(app: FastifyInstance) {
     routePrefix: "/docs",
   });
 }
+
+export const swaggerPlugin = fp(swaggerPluginRaw, {
+  name: "swagger-plugin",
+});
