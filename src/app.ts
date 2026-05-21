b86ef5d4a8bba1ff918799654be25da575994ed9
jgabrieldsl <jgabriel.leite@outlook.com>
2026-05-21 01:24:13 -0300

    fix: health route

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { icmsRoutes } from "./routes/icms";
 import { ipiRoutes } from "./routes/ipi";
 import { nfCompletaRoutes } from "./routes/nf-completa";
 import { pisCofinRoutes } from "./routes/pis-cofins";
+import { healthRoutes } from "./routes/health";
 
 export function buildApp() {
   const app = Fastify({
@@ -15,6 +16,7 @@ export function buildApp() {
   app.register(fastifyCors);
   app.register(swaggerPlugin);
 
+  app.register(healthRoutes);
   app.register(ipiRoutes);
   app.register(pisCofinRoutes);
   app.register(icmsRoutes);
